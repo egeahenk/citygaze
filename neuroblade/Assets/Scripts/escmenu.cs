71f9c72bd7c936788a5c4c256fa827e07002d344
ege <egemenwho@gmail.com>
2023-08-23 14:45:20 +0300

    inventory bugs are fully fixed

--- neuroblade/Assets/Scripts/escmenu.cs
+++ neuroblade/Assets/Scripts/escmenu.cs
@@ -1,25 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.UI;
+using TMPro;
 
 public class escmenu : MonoBehaviour
 {
 
     public GameObject escapePanel;
+    public TextMeshProUGUI orangeCash;
+    
 
     #region escmenu
     
     public void OpenEscapeMenu()
     {
         escapePanel.SetActive(true);
+        orangeCash.gameObject.SetActive(false);
     }
 
     public void CloseEscapeMenu()
     {
         escapePanel.SetActive(false);
+        orangeCash.gameObject.SetActive(true);
     }
 
     #endregion
+    
+    
     void Update()
     {
 
