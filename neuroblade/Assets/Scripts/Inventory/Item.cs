baa62097beed00bd5bdb1fc65f2d44c8dff7eae4
ege <egemenwho@gmail.com>
2023-08-23 21:35:43 +0300

    2nd day added

--- neuroblade/Assets/Scripts/Inventory/Item.cs
+++ neuroblade/Assets/Scripts/Inventory/Item.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum SlotTag { None, Eatable, Food, Paper, Knife,}
+public enum SlotTag { None, Eatable, Knife, CardBoard }
 
 [CreateAssetMenu(menuName = "ScriptableObjects/Item")]
 public class Item : ScriptableObject
--- neuroblade/Assets/Scripts/collectingmoney.cs
+++ neuroblade/Assets/Scripts/collectingmoney.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fixxxx : MonoBehaviour
+public class collectingmoney : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
